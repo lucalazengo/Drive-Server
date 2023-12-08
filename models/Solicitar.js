ba90ed0a0f3083001e11c96b8f223beacc82d249
lucalazengo <lucala@aluno.unilab.edu.br>
2023-12-08 18:33:41 -0300

    Criação das variáveis do formulário

--- models/Solicitar.js
+++ models/Solicitar.js
@@ -2,13 +2,44 @@ const mongoose = require('mongoose');
 
 const SolicitarSchema = new mongoose.Schema({
     status:String,
+    user:String,
+    nome:String,
+    userid:String,
+
+    siape:String,
+    unidade:String,
+    email:String,
+    telefone:String,
+
+
+    atividade:String,
+    objetivo:String,
+    rua:String,
+    bairro:String,
+    cidade:String,
+    referência:String,
+    cep:String,
+    horario_de_partida:String,
+    data_de_partida:String,
+    numero_de_passageiros:Number,
+    parada:String,
+    nome_endereco:String,
+    motivo:String,
+
+
+    comunidade:String,
+
+
+
+
+
     processo:String,
     data:String,
     destino:String,
     capacidade:Number,
-    user:String,
-    user:String,
-    userid:String,
+    
+    
+    
     
 },
     {timestamps: true}
